490b27db7982f20250313d986c5069ac31f1ca1a
atilaholanda <nicolassiqueira700@gmail.com>
2025-11-18 20:57:13 -0300

    COFE

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1 - Criar o menu hambúrguer (div e ícone) no HTML.
+
+// 2 - Fora do @media, aplicar display: none na div que guarda o ícone do hambúrguer 
+//     e também na lista do menu mobile.
+
+// 3 - Dentro do @media, aplicar display: none no menu desktop.
+
+// 4 - Dentro do @media, aplicar display: flex na div do menu mobile que estava oculta, 
+//     tornando-a visível.
+
+// 5 - Realizar a estilização do menu mobile no CSS: alinhamento, cores, transições etc.
+
+// 6 - Fazer o JavaScript interagir com o ícone do menu hambúrguer e executar a ação.
